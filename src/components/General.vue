92e01d28a395e22032804636e918ef6be484a856
Yi Zhen <quekyizhen@gmail.com>
2021-04-19 18:20:36 +0800

    Update General.vue

--- src/components/General.vue
+++ src/components/General.vue
@@ -12,7 +12,7 @@
         <td><p class="qn">Who is Obama?</p> <p class="date">Asked on 10/3/2021</p></td>
     </tr>
     <tr>
-        <td><input type="checkbox"></td>
+        <td><input type="checkbox"><button class="close-image"><img src="@/assets/exclamation-mark.png"></button></td>
         <td><p class="qn">Who is Donald Trump?</p> <p class="date">Asked on 1/3/2021</p></td>
     </tr>
     </table>
@@ -77,6 +77,11 @@ tr:nth-child(odd) {
  background-color: #fff;
 }
 
+input[type=checkbox] {
+    width: 15px;
+    height: 15px;
+    paddin-right: 50px;
+}
 .qn {
     font-size: 20px;
     text-decoration: underline;
@@ -85,72 +90,24 @@ tr:nth-child(odd) {
 .date {
     font-size: 13px;
 }
-
-/* The container */
-.container {
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  border: none;
+  color: #f8f8ff;
+  text-align: center;
+  font-size: 12px;
+  padding: 10px;
+  width: 80px;
+  margin: 5px;
+}
+.close-image {
+  background-color: #f8f8ff;
+  border-color: #f8f8ff;
+}
+.close-image img {
   display: block;
-  position: relative;
-  padding-left: 35px;
-  margin-bottom: 12px;
-  cursor: pointer;
-  font-size: 22px;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-/* Hide the browser's default checkbox */
-.container input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
-}
-
-/* Create a custom checkbox */
-.checkmark {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 25px;
-  width: 25px;
-  background-color: #eee;
-}
-
-/* On mouse-over, add a grey background color */
-.container:hover input ~ .checkmark {
-  background-color: #ccc;
-}
-
-/* When the checkbox is checked, add a blue background */
-.container input:checked ~ .checkmark {
-  background-color: #2196F3;
-}
-
-/* Create the checkmark/indicator (hidden when not checked) */
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
-
-/* Show the checkmark when checked */
-.container input:checked ~ .checkmark:after {
-  display: block;
-}
-
-/* Style the checkmark/indicator */
-.container .checkmark:after {
-  left: 9px;
-  top: 5px;
-  width: 5px;
-  height: 10px;
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  -webkit-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  transform: rotate(45deg);
+  height: 30px;  
+  width: 30px;
 }
 </style>
