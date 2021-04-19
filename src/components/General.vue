394ab8456d2434a659367e0c15d7119ed2178dfc
Yi Zhen <quekyizhen@gmail.com>
2021-04-19 16:15:59 +0800

    Update General.vue
    Added table for question list

--- src/components/General.vue
+++ src/components/General.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-        Home
+<div>
+    <div class="tab">
+        <button class="tablinks">London</button>
+        <button class="tablinks">Paris</button>
+        <button class="tablinks">Tokyo</button>
     </div>
+
+    <table>
+    <tr>
+        <td><p class="qn">Who is Obama?</p> <p class="date">Asked on 10/3/2021</p></td>
+    </tr>
+    <tr>
+        <td><p class="qn">Who is Donald Trump?</p> <p class="date">Asked on 1/3/2021</p></td>
+    </tr>
+    </table>
+</div>
 </template>
 
 <script>
@@ -9,5 +22,66 @@
 </script>
 
 <style>
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
 
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+
+table {
+    width:80%;
+    padding-left: 300px;
+    text-align: left;
+}
+th, td {
+  padding-left: 200px;
+  text-align: left;
+}
+
+tr:nth-child(even) {
+  background-color: #f8f8ff;
+}
+tr:nth-child(odd) {
+ background-color: #fff;
+}
+
+.qn {
+    font-size: 20px;
+    text-decoration: underline;
+}
+
+.date {
+    font-size: 13px;
+}
 </style>
